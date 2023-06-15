385a18bf7d1ef4b6b3158637edb54b1eb3759c95
Liam-M-Dev <liammassey21@gmail.com>
2023-06-15 15:42:24 +1000

    Set up db connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
+
+async function dbConnect(){
+	try {
+		await mongoose.connect("mongodb://localhost:27017/note_taking_db")
+		console.log("Database Connected")
+	} catch (error) {
+		console.log("There was an error connecting the database:\n" + JSON.stringify(error))
+	}
+};
+
+dbConnect();
 
 app.get("/", (request, response) => {
 	response.json({
